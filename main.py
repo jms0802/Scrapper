7751d90d2c601045d158a7dd7a21ce2f24ebd228
Nytail <>
2021-05-02 13:47:33 +0000

    3 사용자에게 인풋값을 받고 인풋값을 출력하는 법을 배웠다.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("Scrapper")
 
 @app.route("/")
 def home():
-  return "<h1>Job Serch</h1>"
+  return render_template("home.html")
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  return render_template("report.html", searchingBy=word)
 
 app.run(host="0.0.0.0")
