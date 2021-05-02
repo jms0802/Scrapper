2c2c45437e7177b7d16e8a6532064d3c25c8d402
Nytail <>
2021-05-02 14:19:14 +0000

    4 파이썬으로 만든 스크랩퍼 코드를 가져와서 적용해주고, 같은 단어가 입력되었을 시 다시 검샘하는게 아닌 저장된 값을 출력할 수 있게 했다.(db={}) , 검색 결과 갯수도 출력 가능하다.

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("Scrapper")
 
+db = {}
+
+
 @app.route("/")
 def home():
-  return render_template("home.html")
+    return render_template("home.html")
+
 
 @app.route("/report")
 def report():
-  word = request.args.get('word')
-  return render_template("report.html", searchingBy=word)
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        fromDb = db.get(word)
+        if fromDb:
+            jobs = fromDb
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+      "report.html",
+      searchingBy=word,
+      resultNumber=len(jobs),
+      jobs=jobs
+    )
+
 
 app.run(host="0.0.0.0")
